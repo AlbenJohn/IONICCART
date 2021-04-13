9735dde30b1a84dbb45a7eab039726f02a487705
ESAT5 <ESAT5@ESAT5>
2021-04-13 17:22:30 +0400

    Third Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,10 +8,17 @@ import { Geolocation, GeolocationOptions, Geoposition, PositionError } from '@io
 })
 export class HomePage {
 
-  public Categories:any =[{name:"A",image:'',index:""},
+  public avatars:any =[{name:"A",image:'',index:""},
+  {name:"",image:'',index:""},{name:"",image:'',index:""},{name:"",image:'',index:""},
+  {name:"A",image:'',index:""},
   {name:"",image:'',index:""},{name:"",image:'',index:""},{name:"",image:'',index:""}];
   options: GeolocationOptions;
     currentPos: Geoposition;
+    slideOptsOne = {
+      initialSlide: 0,
+      slidesPerView: 1,
+      autoplay:true
+     }
   constructor(private geolocation: Geolocation) {
     this.getUserPosition();
   }
